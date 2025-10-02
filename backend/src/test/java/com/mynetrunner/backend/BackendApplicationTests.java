2be34060afa370793b196344258f34c16e5c9892
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2025-10-01 21:30:18 -0700

    intial springboot initialization

---
+++ backend/src/test/java/com/mynetrunner/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mynetrunner.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
